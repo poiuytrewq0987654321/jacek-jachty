d50cb560166d2a4efe4b7ddba93de8eb32b7864c
poiuytrewq0987654321 <bundushathurh@gmail.com>
2021-12-30 15:36:47 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,38 +1,31 @@
-$(document).ready(function () {
-  
-    // Use the given data to create 
-    // the table and display it
-    $('table').bootstrapTable({
-      data: mydata
-    });
-  });
-    var mydata = 
-[
-    {
-        "obraz": "<a href='images/1.jpg'>link do obrazu</a>",
-        "id": 1,
-        "stocznia": "Jeanneau",
-        "model": "NC 14",
-        "kategoria": "jacht motorowy",
-        "lokalizacja": "Murter",
-        "dlugosc": 14,
-        "szerokosc": 4.1,
-        "rok_produkcji": 2017,
-        "liczba_kabin": 2,
-        "moc_silnika": "370 KM"
-      },
-      {
-        "obraz": "<a href='images/2.jpg'>link do obrazu</a>",
-        "id": 2,
-        "stocznia": "test",
-        "model": "NC 14",
-        "kategoria": "jacht motorowy",
-        "lokalizacja": "test",
-        "dlugosc": 14,
-        "szerokosc": 4.1,
-        "rok_produkcji": 2017,
-        "liczba_kabin": 2,
-        "moc_silnika": "370 KM"
-      }
-];
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
 
+//usage:
+readTextFile("test.json", function(text){
+    var mydata = JSON.parse(text);
+    $(document).ready(function () {
+  
+        // Use the given data to create 
+        // the table and display it
+        $('table').bootstrapTable({
+          data: mydata
+        });
+      });
+});
+
+function wyslijMaila() {
+    var mail = document.getElementById("exampleFormControlInput1").value;
+    var jacht = document.getElementById("exampleFormControlSelect1").value;
+    var pytania = document.getElementById("exampleFormControlTextarea1").value;
+    return confirm ('wyslales formularz\ntwoj mail to: ' + mail + '\nwybrany numer jachtu to: ' + jacht + '\ndodatkowe pytania to: ' + pytania);
+  }
